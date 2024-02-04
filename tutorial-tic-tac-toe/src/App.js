a3f7189c5192db5d4ced34bb0e5ca7ed4f722ec1
leloufadel <leloufadel@gmail.com>
2024-02-04 23:24:34 +0000

    update the Board component to render Square component using JSX syntax

--- tutorial-tic-tac-toe/src/App.js
+++ tutorial-tic-tac-toe/src/App.js
@@ -3,25 +3,29 @@
 // // import './styles.css';
 
 // import App from './App';
-
+function Square() {
+  return <button className="square">1</button>;
+}
 export default function Board() {
   return(
   <> 
   <div className="board-row">
+  <Square/>
+  <Square/>
+  <Square/>
 
-  <button className="square">1</button>
-  <button className="square">2</button>
-  <button className="square">3</button>
   </div>
   <div className="board-row">
 
-  <button className="square">4</button>
-  <button className="square">5</button>
-  <button className="square">6</button>
+  <Square/>
+  <Square/>
+  <Square/>
+
   </div>
-  <button className="square">7</button>
-  <button className="square">8</button>
-  <button className="square">9</button>
+  <Square/>
+  <Square/>
+  <Square/>
+
 
   </>
   )
