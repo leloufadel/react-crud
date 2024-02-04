5df4815dfb1f3a1c520c18fd9169c6025b8ea452
leloufadel <leloufadel@gmail.com>
2024-02-04 22:20:27 +0000

    Add a square button

--- tutorial-tic-tac-toe/src/App.js
+++ tutorial-tic-tac-toe/src/App.js
@@ -1,3 +1,8 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+
+import App from './App';
 export default function Square() {
-  return <button className="square">X</button>;
+  return  <button className="square">X</button>;
 }
