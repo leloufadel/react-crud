60c1d8f8ccbeab1cc7d39e89f198b65027ac938f
leloufadel <leloufadel@gmail.com>
2024-02-04 23:38:34 +0000

    pass the value prop to each Square component

--- tutorial-tic-tac-toe/src/App.js
+++ tutorial-tic-tac-toe/src/App.js
@@ -3,28 +3,28 @@
 // // import './styles.css';
 
 // import App from './App';
-function Square() {
-  return <button className="square">1</button>;
+function Square({ value }) {
+  return <button className="square">{value}</button>;
 }
 export default function Board() {
   return(
   <> 
   <div className="board-row">
-  <Square/>
-  <Square/>
-  <Square/>
+  <Square value="1"/>
+  <Square value="2"/>
+  <Square value="3"/>
 
   </div>
   <div className="board-row">
 
-  <Square/>
-  <Square/>
-  <Square/>
+  <Square value="4"/>
+  <Square value="5"/>
+  <Square value="6"/>
 
   </div>
-  <Square/>
-  <Square/>
-  <Square/>
+  <Square value="7 "/>
+  <Square value="8"/>
+  <Square value="9"/>
 
 
   </>
